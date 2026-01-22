cb0749130295c2ba79302b69d7bdcfc173727a54
johnmarsden24 <john.marsden24@gmail.com>
2026-01-22 14:25:53 +0000

    fixed import

--- src/components/ProductActions/index.tsx
+++ src/components/ProductActions/index.tsx
@@ -1,5 +1,5 @@
 import { useProduct } from '../../hooks/useProduct';
-import { useCartContext } from '../../contexts/CartContext';
+import { useCartContext } from '../../contexts/useCartContext';
 import { Button } from '../ui/Button';
 import styles from './ProductActions.module.css';
 
